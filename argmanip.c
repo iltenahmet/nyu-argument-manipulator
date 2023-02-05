e0d639d11e2888afd999e937a2e1466a4238df4c
iltenahmet <55655359+iltenahmet@users.noreply.github.com>
2023-02-05 12:51:20 -0500

    Initial setup so that the program compiles.

--- argmanip.c
+++ argmanip.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
 #include "argmanip.h"
 
+char **manipulate_args(int argc, const char *const *argv, int (*const manip)(int))
+{
+    printf("-Manipulate Args!-");
+    (void)argc;
+    (void)argv;
+    (void)manip;
+    char c = 'c';
+    char* pointer = &c;
+    char** pointer2 = &pointer;
+    return pointer2;
+}
+
+void free_copied_args(char **args, ...)
+{
+    printf("-Free copied args!-");
+    (void)args;
+}

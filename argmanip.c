0b2670ad06f30651919230b1e971a95813981ade
iltenahmet <55655359+iltenahmet@users.noreply.github.com>
2023-02-05 13:08:20 -0500

    Worked on manipulate_args function

--- argmanip.c
+++ argmanip.c
@@ -3,14 +3,7 @@
 
 char **manipulate_args(int argc, const char *const *argv, int (*const manip)(int))
 {
-    printf("-Manipulate Args!-");
-    (void)argc;
-    (void)argv;
-    (void)manip;
-    char c = 'c';
-    char* pointer = &c;
-    char** pointer2 = &pointer;
-    return pointer2;
+    char** newArgs = (char**)malloc(argc * sizeof(char*));
 }
 
 void free_copied_args(char **args, ...)

00da53f79deebed5190b024803cb3b2c78a0575d
root <root@LAPTOP-C3CSHMT8>
2023-02-06 19:45:41 -0500

    working manipulate_args and free_copied_args functions, need to resolve segmentation fault

--- argmanip.c
+++ argmanip.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 #include "argmanip.h"
 
 char **manipulate_args(int argc, const char *const *argv, int (*const manip)(int))
 {
-    char** newArgs = (char**)malloc(argc * sizeof(char*));
+    char** manipulatedArgs = (char**) malloc(argc * sizeof(char*));
+
+    for (int i = 0; i < argc; i++)
+    {
+        const char* oldStr = argv[i];
+        int length = strlen(oldStr) + 1;
+        char* newStr = (char*)malloc(length);
+
+        for (int j = 0; j < length; j++)
+        {
+            newStr[j] = manip(oldStr[j]);
+        }
+
+        manipulatedArgs[i] = newStr;
+    }
+
+    return manipulatedArgs;
 }
 
 void free_copied_args(char **args, ...)
 {
-    printf("-Free copied args!-");
-    (void)args;
+    va_list list;
+    va_start(list, args);
+
+    char** current = args;
+    while (current != NULL)
+    {
+        for(int i = 0; current[i] != NULL; i++)
+        {
+            free(current[i]);
+        }
+        free(current);
+        current = va_arg(list, char**);
+    }
+
+    va_end(list);
 }
